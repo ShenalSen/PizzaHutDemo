94a113aaf4028050942d3d05a2fb7d9215990e52
ShenalSen <shenal.unc555@gmail.com>
2025-07-01 18:48:37 +0530

    Implement MenuService class to load pizza menu from menuList.json

---
+++ src/main/java/org/pizzastore/MenuService.java
@@ -0,0 +1,33 @@
+package org.pizzastore;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class MenuService {
+    private List<Pizza> pizzas;
+
+    public MenuService() {
+        loadMenu();
+    }
+
+    private void loadMenu() {
+        try {
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("menuList.json");
+            if (inputStream == null) {
+                throw new RuntimeException("Could not find menuList.json");
+            }
+            
+            Gson gson = new Gson();
+            Type listType = new TypeToken<List<Pizza>>(){}.getType();
+            pizzas = gson.fromJson(new InputStreamReader(inputStream), listType);
+        } catch (Exception e) {
+            throw new RuntimeException("Error loading menu from menuList.json: " + e.getMessage(), e);
+        }
+    }
+
+}
