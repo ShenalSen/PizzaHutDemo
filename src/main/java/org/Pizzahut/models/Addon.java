e12f162dcb6955563126ea9c515dd90d6c0de167
ShenalSen <shenal.unc555@gmail.com>
2025-07-07 13:45:19 +0530

    Refactor Addon class to improve code organization and enhance business methods

--- src/main/java/org/Pizzahut/models/Addon.java
+++ src/main/java/org/Pizzahut/models/Addon.java
@@ -19,4 +19,46 @@ public class Addon {
         this.category = category;
     }
 
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public MenuCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(MenuCategory category) {
+        this.category = category;
+    }
+
+    
+    public String getFormattedPrice() {
+        return String.format("%.2f LKR", price);
+    }
+
+    public boolean isApplicableToCategory(MenuCategory targetCategory) {
+        return this.category == targetCategory;
+    }
+
 }
