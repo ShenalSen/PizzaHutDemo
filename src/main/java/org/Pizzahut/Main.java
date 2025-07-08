2640f4d9371a181009e1622af6f40598fb89d1ee
ShenalSen <shenal.unc555@gmail.com>
2025-07-08 17:44:09 +0530

    Initialize MenuController in main method to start the application

--- src/main/java/org/Pizzahut/Main.java
+++ src/main/java/org/Pizzahut/Main.java
@@ -1,5 +1,10 @@
 package org.Pizzahut;
 
+import org.Pizzahut.ui.MenuController;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        MenuController controller = new MenuController();
+        controller.start();
+    }
 }
